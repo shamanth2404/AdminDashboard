2b09760db95354787494a21828417309b097ee81
Shamanth K Murthy <shamanthk2404@gmail.com>
2023-12-25 09:42:13 +0530

    calendar

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Team from './pages/team/Team'
 import Contacts from "./pages/contacts/Contacts";
 import Invoices from "./pages/invoices/Invoices";
 import Form from "./pages/form/Form";
+import Calendar from "./pages/calendar/Calendar";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -27,6 +28,7 @@ function App() {
                 <Route path="/contacts" element={<Contacts />} />
                 <Route path="/invoices" element={<Invoices />} />
                 <Route path="/form" element={<Form />} />
+                <Route path="/calendar" element={<Calendar />} />
             </Routes>
           </main>
         </div>
